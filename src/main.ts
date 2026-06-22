30e2c0965daae1251ec32a0dca0e1809f916200a
Pedro Moreno <pgmorenow9314@gmail.com>
2026-06-22 18:47:20 -0400

    feat(app): wire ConfigModule, HealthModule, global filter and interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from './config/config.service';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  const port = configService.get('port');
+  await app.listen(port);
 }
-bootstrap();
+
+void bootstrap();
